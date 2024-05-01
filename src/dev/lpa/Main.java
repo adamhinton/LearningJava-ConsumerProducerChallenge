3534cc3e90dbf6914107cbcf72ce4393c64b1d61
adamd <adam.d.hinton@gmail.com>
2024-04-30 21:22:46 -0400

    mostly finished

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,6 +1,12 @@
 package dev.lpa;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 record Order(long orderId, String item, int qty) {
 };
@@ -17,6 +23,37 @@ public class Main {
 
         ShoeWarehouse warehouse = new ShoeWarehouse();
 
+        ExecutorService orderingService = Executors.newCachedThreadPool();
+
+        Callable<Order> orderingTask = () ->{
+            {
+                Order newOrder = generateOrder();
+                try{
+                    Thread.sleep(random.nextInt(500, 5000));
+                    warehouse.receiveOrder(newOrder);
+                }
+                catch(InterruptedException e){
+                    throw new RuntimeException(e);
+                }
+                return newOrder;
+            }
+        };
+
+        List<Callable<Order>> tasks = Collections.nCopies(15, orderingTask);
+        try {
+            orderingService.invokeAll(tasks);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        orderingService.shutdown();
+        try {
+            orderingService.awaitTermination(7, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        warehouse.shutDown();
 
     }
 
